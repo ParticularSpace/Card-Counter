15cda2fa9cde5fa6304e1eb0a21e5a590e70c90b
Sam Jones <samejones2018@gmail.com>
2023-04-01 00:15:24 -0700

    created a function to keep track of hand worth so we could controle actions in the game

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,6 +14,23 @@ const cardValue = {
   "A": -1
 };
 
+const cardWorth = {
+  "2": 2,
+  "3": 3,
+  "4": 4,
+  "5": 5,
+  "6": 6,
+  "7": 7,
+  "8": 8,
+  "9": 9,
+  "10": 10,
+  "J": 10,
+  "Q": 10,
+  "K": 10,
+  "A": 11
+};
+
+
 // An array to hold all the cards the user has selected for the game so far
 var cards = [];
 
@@ -24,13 +41,41 @@ $(document).ready(function () {
   var dealerUpCard = [];
   var dealerDownCard = [];
   var trueCount = 0;
+  let playerHandTotal = 0;
+  let dealerHandTotal = 0;
+
+  function calculateHandTotal(hand) {
+    var total = 0;
+    var aces = 0;
+  
+    for (var i = 0; i < hand.length; i++) {
+      var card = hand[i];
+      var worth = cardWorth[card];
+      
+      if (card === "A") {
+        aces += 1;
+        total += worth;
+      } else {
+        total += worth;
+      }
+  
+      while (total > 21 && aces > 0) {
+        total -= 10;
+        aces--;
+      }
+    }
+  
+    return total;
+  }
+  
+  
 
 
   function getTrueCount(cards, numDecks) {
     var liveCount = 0;
     for (var i = 0; i < cards.length; i++) {
 
-      liveCount += cardValue[cards[i]]; 
+      liveCount += cardValue[cards[i]];
 
     }
     var trueCount = liveCount / numDecks;
@@ -40,44 +85,54 @@ $(document).ready(function () {
   $("#submit-cards-btn").on("click", function () {
     var $dealerUpCard = $("#upCard").val();
     var $dealerDownCard = $("#downCard").val();
-  
+
     var card1 = $("#cardOne").val();
     var card2 = $("#cardTwo").val();
-  
-    cardsInHand = [card1, card2];
+
+    cardsInHand.push(card1);
+    cardsInHand.push(card2);
+    playerHandTotal = calculateHandTotal(cardsInHand);
+    console.log("Player hand total:", playerHandTotal);
+
     dealerUpCard = [$dealerUpCard];
-  
+
     // Update the cards array
     cards = cards.concat(cardsInHand).concat(dealerUpCard);
-  
+
     //console log the cards
     console.log('Your hand is: ' + card1 + ' , ' + card2 + ' Dealers hand is ' + $dealerUpCard);
-  
-    //Dealer known card
+
     dealerUpCard.push($dealerUpCard);
-  
+    dealerHandTotal = calculateHandTotal(dealerUpCard);
+    console.log("Dealer hand total:", dealerHandTotal);
+
+
     //will be unknown for now
     dealerDownCard.push($dealerDownCard);
-  
+
     // Update the history display
     updateHistoryDisplay();
-  
+
     // Update the live count
     trueCount = getTrueCount(cards, numDecks);
     $("#countDisplay").text(trueCount);
   });
-  
-  
-  
+
+
+
 
   // When the user clicks the "Hit" button, add the card to the cards in hand and update the count
   $("#submit-hit-card-btn").on("click", function () {
-  
+
 
     var hitCard = $("#hitCard").val();
 
     if (hitCard) {
       cards.push(hitCard);
+      cardsInHand.push(hitCard);
+      playerHandTotal = calculateHandTotal(cardsInHand);
+      console.log("Player hand total:", playerHandTotal);
+
 
 
       //update live count on page
@@ -99,6 +154,11 @@ $(document).ready(function () {
     if (hiddenCard) {
 
       cards.push(hiddenCard);
+      dealerDownCard.push(hiddenCard);
+      dealerUpCard.push(hiddenCard);
+      dealerHandTotal = calculateHandTotal(dealerUpCard);
+      console.log("Dealer hand total:", dealerHandTotal);
+
 
       trueCount = getTrueCount(cards, numDecks);
       $("#countDisplay").text(trueCount);
@@ -123,7 +183,7 @@ $(document).ready(function () {
     updateHistoryDisplay();
   });
 
-  function updateHistoryDisplay () {
+  function updateHistoryDisplay() {
 
     $("#history").empty();
 
